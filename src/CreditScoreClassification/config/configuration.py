c8b0626f6a717e93e8cc684370197f10cb41bee0
augustin <augustin7766@gmail.com>
2024-06-08 22:10:45 +0530

    Data_Validation

--- src/CreditScoreClassification/config/configuration.py
+++ src/CreditScoreClassification/config/configuration.py
@@ -2,7 +2,7 @@
 
 from src.CreditScoreClassification.constants import *
 from src.CreditScoreClassification.utils.common import read_yaml, create_directories
-from src.CreditScoreClassification.entity.config_entity import (DataIngestionConfig)
+from src.CreditScoreClassification.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 from src.CreditScoreClassification.logger_file.logger_obj import logger
 
@@ -39,3 +39,22 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            local_data_file= config.local_data_file,
+            STATUS_FILE = config.STATUS_FILE,
+            all_schema = schema
+        )
+
+        return data_validation_config
